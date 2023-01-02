4dc82002ecc9bf890a03ec46761e5fa1ab652786
nofr22pl <112422422+nofr22pl@users.noreply.github.com>
2023-01-02 03:03:44 +0100

    Updated

--- westcoast-education.web/Controllers/CoursesAdminController.cs
+++ westcoast-education.web/Controllers/CoursesAdminController.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using westcoast_education.web.Data;
+using westcoast_education.web.Models;
 
 namespace westcoast_education.web.Controllers
+
+
 {
     [Route("coursesadmin")]
     public class CoursesAdminController : Controller
@@ -10,7 +13,6 @@ namespace westcoast_education.web.Controllers
         private readonly WestcoastEducationContext _context;
         public CoursesAdminController(WestcoastEducationContext context)
         {
-            
             _context = context;
         }
 
@@ -18,6 +20,21 @@ namespace westcoast_education.web.Controllers
         {
             var courses = await _context.Courses.ToListAsync();
             return View("Index", courses);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            var course = new Course();
+            return View("Create", course);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(Course course)
+        {
+            await _context.Courses.AddAsync(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         
