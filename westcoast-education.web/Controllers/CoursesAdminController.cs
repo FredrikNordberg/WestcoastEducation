712afacfa429f3297c3ec0934627b692477512ea
nofr22pl <112422422+nofr22pl@users.noreply.github.com>
2023-01-03 20:21:16 +0100

    Added Error Messeges

--- westcoast-education.web/Controllers/CoursesAdminController.cs
+++ westcoast-education.web/Controllers/CoursesAdminController.cs
@@ -18,8 +18,23 @@ namespace westcoast_education.web.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var courses = await _context.Courses.ToListAsync();
-            return View("Index", courses);
+            try
+            {
+                var courses = await _context.Courses.ToListAsync();
+                return View("Index", courses);
+            }
+            catch (Exception ex)
+            {
+                
+                var error = new ErrorModel{
+                    ErrorTitle = "Ett fel har inträffat när vi skulle hämta alla bilar",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
+            
+            
         }
 
         [HttpGet("create")]
@@ -32,9 +47,130 @@ namespace westcoast_education.web.Controllers
         [HttpPost("create")]
         public async Task<IActionResult> Create(Course course)
         {
+            try
+            {
+                var exists = await _context.Courses.SingleOrDefaultAsync(c => c.CourseId == course.CourseId);
+
+            if (exists is not null)
+            {
+                var error = new ErrorModel
+                {
+                    ErrorTitle ="Ett fel har inträffat när kursen skulle sparas!",
+                    ErrorMessage = $"En kurs med kursnummer {course.CourseId} finns redan i systemet"
+                };
+
+                return View("_Error", error);
+            }
+
             await _context.Courses.AddAsync(course);
             await _context.SaveChangesAsync();
+            
             return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                
+                var error = new ErrorModel{
+                    ErrorTitle = "Ett fel har inträffat när vi skulle spara kursen",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
+            
+
+            
+        }
+         
+         
+          [HttpGet("edit/{courseId}")]
+        public async Task<IActionResult> Edit(int courseId)
+        {
+            try
+            {
+                var course = await  _context.Courses.SingleOrDefaultAsync(c => c.CourseId == courseId);
+
+            if (course is not null) return View("Edit", course);
+
+            var error = new ErrorModel{
+                ErrorTitle = "Ett fel har inträffat när vi skulle hämta en kurs för redegering",
+                ErrorMessage = $"Vi hittar ingen kurs med id {courseId}"
+            };
+
+            return View("_Error", error);
+            }
+            catch (Exception ex)
+            {
+                
+                var error = new ErrorModel{
+                    ErrorTitle = "Ett fel har inträffat när vi hämtar kurs för redegering",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error); 
+            }
+            
+            
+            
+        }
+
+        [HttpPost("edit/{courseId}")]
+        public async Task<IActionResult> Edit(int courseId, Course course)
+        {
+            try
+            {
+                var courseToUpdate = _context.Courses.SingleOrDefault(c => c.CourseId == courseId);
+
+                if(courseToUpdate is null) return RedirectToAction(nameof(Index));
+
+                courseToUpdate.CourseId = course.CourseId;
+                courseToUpdate.CourseName = course.CourseName;
+                courseToUpdate.CourseLocation = course.CourseLocation;
+                courseToUpdate.CourseLanguage = course.CourseLanguage;
+
+                _context.Courses.Update(courseToUpdate);
+                await _context.SaveChangesAsync();
+            
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                
+                 var error = new ErrorModel{
+                    ErrorTitle = "Ett fel har inträffat när vi skulle spara kursen",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
+            
+        }
+
+        [Route("delete/{courseId}")]
+        public async Task<IActionResult> Delete(int courseId)
+        {
+            try
+            {
+                var courseToDelete = await _context.Courses.SingleOrDefaultAsync(c => c.CourseId == courseId);
+
+                if(courseToDelete is null) return RedirectToAction(nameof(Index));
+
+                _context.Courses.Remove(courseToDelete);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                
+               var error = new ErrorModel{
+                    ErrorTitle = "Ett fel har inträffat när kursen skulle raderas",
+                    ErrorMessage = ex.Message
+                };
+
+                return View("_Error", error);
+            }
+            
         }
 
         
--- westcoast-education.web/Models/Course.cs
+++ westcoast-education.web/Models/Course.cs
@@ -2,7 +2,7 @@ namespace westcoast_education.web.Models
 {
     public class Course
     {
-        public int CourseId {get; set;}
+        public int CourseId {get; set;} 
         public string CourseName { get; set;} = "";
         public string CourseLocation { get; set; } = "";
         public string CourseLanguage { get; set; } = "";
